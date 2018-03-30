fd855408f2ddade38ed11efeb4839a106e7579d0
osd-user2 <34326639+osd-user2@users.noreply.github.com>
2018-03-30 15:24:12 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Resource test"
+    return "Network test to Project1"
 
 if __name__ == "__main__":
     application.run()
